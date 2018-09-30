9d634b4e2d5324e41374bab046bbcff17983df47
greenline <gilles.pinchart@gmail.com>
2018-09-30 16:32:40 +0200

    add

--- MVC/Appli/snippets/execGit.php
+++
@@ -1,8 +0,0 @@
-<?php
-$path = "/var/www/repos/$_POST[project]"; 
-$a='';
-chdir($path);
-exec("git add .");  
-exec("git commit -m'message'");
-echo "<h3 align = center> Succesfully commited all the files.</h3>";
-?>
